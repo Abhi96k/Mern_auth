2042048f5e03b5eab1dc7c05ecc9103746bd031a
abhi@96k <abhisheknangare96k@gmail.com>
2024-10-09 22:47:31 +0530

    Implement signup and login functionality with validation and error handling

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,60 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import { User } from "../models/user.model";
+
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const userAlreadyExists = await User.findOne({ email });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const user=await User.
+
+  } catch (error) {
+    console.log("there is issue for login");
+    console.log("Error", error);
+  }
 };
 
 export const logout = (req, res) => {
